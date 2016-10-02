df963efc7f2d27bf04b026c56873c300fdd02908
Gautam Dey <gautam@deyfamily.org>
2016-10-01 22:41:09 -0700

    Initial Commit.
    Basic functionally is working. There is a way to specify only a set of test cases to be run.
    
    Not implemented, teardown and setup functions or fixtures as they are currently unneeded.

---
+++ README.md
@@ -0,0 +1,41 @@
+# tbl
+
+This is a simple package to help with table driven tests. Given a set
+of test cases, it will call the provided function each test case.
+
+This helps remove boiler plate that comes with writing table driven code.
+
+API [documentation](https://godoc.org/github.com/gdey/tbl).
+
+## Example
+
+```go
+
+func TestFoo(t *testing.T) {
+    type testcase struct{
+        p bool
+       expected bool
+    }
+    test := tlb.Cases(
+        testcaase{
+            p : true,
+            expected : true,
+        },
+   //…
+    )
+    test.Run(func(idx int, tc testcase)bool{
+        if foo.IsGood(tc.p) != tc.expected  {
+            t.Errorf("Test %v failed",idx)
+        }
+        return true // Run the next test case.
+    })
+}
+```
+
+# command line flags
+
+In addition, the tool adds a new command line flag to help with debugging.
+
+`--tblTest.RunOrder` : Allows one to specify the testcases's and the order they should run in.
+This is usually helpful, when you are trying to fix one failing test, that you want to keep running
+over and over again.
---
+++ tbl_test.go
@@ -0,0 +1,68 @@
+// Copyright 2016 Gautam Dey. All rights reserved.
+// Use of this source code is governed by FreeBDS License (2-clause Simplified BSD.)
+// that can be found in the LICENSE file.
+
+package tbl_test
+
+import (
+	"testing"
+
+	"github.com/gdey/tbl"
+)
+
+func TestCases(t *testing.T) {
+	type testcase struct {
+		val  int
+		next bool
+	}
+	test := tbl.Cases(
+		testcase{
+			val:  0,
+			next: true,
+		},
+		testcase{
+			val:  1,
+			next: false,
+		},
+		testcase{
+			val:  2,
+			next: true,
+		},
+	)
+	count := test.Run(func(tc testcase) {})
+	if count != 3 {
+		t.Errorf("did not run all the testcases.")
+	}
+
+	count = test.Run(func(tc testcase) bool {
+		return tc.next
+	})
+	if count != 2 {
+		t.Errorf("expected to only run two test. ran %v instead", count)
+	}
+	count = test.Run(func(idx int, tc testcase) {
+		if tc.val != idx {
+			t.Errorf("for test %v: expected %[1]v, got %v", idx, tc.val)
+		}
+	})
+	if count != 3 {
+		t.Errorf("did not run all the testcases.")
+	}
+	count = test.Run(func(idx int, tc testcase) bool {
+		if tc.val != idx {
+			t.Errorf("for test %v: expected %[1]v, got %v", idx, tc.val)
+		}
+		return tc.next
+	})
+	if count != 2 {
+		t.Errorf("expected to only run two test. ran %v instead", count)
+	}
+}
+
+func TestIntCases(t *testing.T) {
+	test := tbl.Cases(0, 1, 2, 3)
+	count := test.Run(func(tc int) {})
+	if count != 4 {
+		t.Errorf("did not run all the testcases.")
+	}
+}
